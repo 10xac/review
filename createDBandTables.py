4514769d0f316981cca3e2235b3970648e7257aa
Jabor047 <gkkarobia@gmail.com>
2021-06-11 11:22:47 +0300

    added createTables, dbExecuteQuery and showTables functions

--- createDBandTables.py
+++ createDBandTables.py
@@ -30,31 +30,88 @@ def get_secret(secret_name="b4test-mysql", region_name="eu-west-1"):
 
     return json.loads(secret)
 
-def createDB():
+def DBConnect(dbName=None):
     dbauth = get_secret()
 
-    mydb = mysql.connect(host=dbauth['host'], user=dbauth['username'], password=dbauth['password'],
-                         buffered=True)
+    conn = mysql.connect(host=dbauth['host'], user=dbauth['username'], password=dbauth['password'],
+                         database=dbName, buffered=True)
 
-    cur = mydb.cursor()
+    cur = conn.cursor()
+    return conn, cur
+
+def createDB():
+    conn, cur = DBConnect()
     cur.execute("CREATE DATABASE review")
-    mydb.commit()
+    conn.commit()
     cur.close()
 
 def showDbs():
-    dbauth = get_secret()
-    mydb = mysql.connect(host=dbauth['host'], user=dbauth['username'], password=dbauth['password'],
-                         buffered=True)
-
-    cursor = mydb.cursor()
-    cursor.execute("SHOW DATABASES")
+    conn, cur = DBConnect()
+    cur.execute("SHOW DATABASES")
     res = []
-    for (databases) in cursor:
-        res.append(databases[0].decode("utf-8"))
+    for (databases) in cur:
+        res.append(databases[0])
 
     df = pd.DataFrame(res, columns=['Database Names'])
 
     print(df)
 
-createDB()
-showDbs()
+def createTables():
+    conn, cur = DBConnect()
+    sqlFile = 'createMYSQLTables.sql'
+    fd = open(sqlFile, 'r')
+    readSqlFile = fd.read()
+    fd.close()
+
+    sqlCommands = readSqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            res = cur.execute(command)
+        except Exception as ex:
+            print("Command skipped: ", command)
+            print(ex)
+    conn.commit()
+    cur.close()
+
+def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs):
+    connection, cursor1 = DBConnect(**kwargs)
+    if many:
+        cursor1.executemany(*args)
+    else:
+        cursor1.execute(*args)
+
+    # get column names
+    field_names = [i[0] for i in cursor1.description]
+
+    # get column values
+    res = cursor1.fetchall()
+
+    # get row count and show info
+    nrow = cursor1.rowcount
+    if tablename:
+        print(f"{nrow} recrods fetched from {tablename} table")
+
+    cursor1.close()
+    connection.close()
+
+    # return result
+    if rdf:
+        return pd.DataFrame(res, columns=field_names)
+    else:
+        return res
+
+def showTables(q=None, **kwargs):
+    if q is None:
+        q = 'SHOW TABLES'
+
+    rdf = kwargs.pop('rdf', False)
+
+    res = db_execute_fetch(q, rdf=False, **kwargs)
+    res = [t[0].decode("utf-8") for t in res]
+    df = pd.DataFrame(res, columns=['Table Names'])
+
+    return df
+
+createTables()
+showTables()
